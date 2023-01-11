6f92e2c9e8e4478265c029c8b36b992e560c4c6b
husky022 <balashovia@bk.ru>
2023-01-11 14:04:10 +0300

    Initial commit

---
+++ iqworks/secrets_sample.py
@@ -0,0 +1,11 @@
+# secret key for django
+
+secret_key = '**********************************************************'
+
+# database settings
+
+db_name = 'db_name'
+db_user = 'db_user'
+db_password = 'db_password'
+db_host = 'db_host'
+db_port = 'db_port'
