b99be79f4d64fbf006eddc809693c64fa41bdcaf
husky022 <balashovia@bk.ru>
2023-01-13 21:33:03 +0300

    added celery

--- mainapp/tasks.py
+++ mainapp/tasks.py
@@ -1,11 +1,9 @@
-from time import sleep
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from celery import shared_task
 
 @shared_task()
 def send_email_task(name, mail_client, mail_iqw, phone):
-    sleep(5)  # Simulate expensive operation(s) that freeze Django
     client = f'Имя: ' + name + 'Почта: ' + mail_client + 'Телефон: ' + phone
     send_mail('Новая заявка', client, mail_iqw, [mail_iqw])
     data = {'name': name, 'mail': mail_client, 'phone': phone}
